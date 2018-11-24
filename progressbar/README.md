085ace6cdd54de84518a3e603dff197e7974477b
wkstudy <1346948470@qq.com>
2018-11-24 18:25:11 +0800

    完成进度条

---
+++ progressbar/README.md
@@ -0,0 +1,15 @@
+## 进度条
+[demo](https://wkstudy.github.io/smallplugs/progressbar/index.html)
+
+
+### knowledge
+* z-index只对定位元素有效
+*  关于定时器的使用，自己了解的太少，需要参考[阮一峰](http://javascript.ruanyifeng.com/advanced/timer.html)
+
+> * 比如定时器执行环境为全局
+> * 参数的传递（apply、匿名函数、bind方法），都是用来解决定时器的执行环境为全局的问题
+> * 防抖技术
+
+### 注意
+* 自己原来将change函数放在外面定义，导致t不被识别，所以定义到了里面
+* 使用obj.num而不适用num的原因是，对象传递的引用，而基本类型（Undefined、Null、Boolean、Number、String 5种基本数据类型）是进行复制。
---
+++ progressbar/index.js
@@ -0,0 +1,19 @@
+function move () {
+  var bar = document.getElementById('bar'),
+      txt = document.getElementById('txt'),
+      obj = {};
+      obj.num = 0;
+  var t = setInterval(change, 100, bar, txt, obj);
+  function change (bar, txt, obj) {
+    if (obj.num <= 100) {
+      bar.style.width = obj.num + '%';
+      txt.innerHTML = obj.num + '%';
+      obj.num++;
+    }else {
+      clearInterval(t);
+    }
+   
+  }
+}
+
+move();
